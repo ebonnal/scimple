b84fb8974405c50470e32643744a56edc631037c
Bonnal Enzo <enzo.bonnal@orange.com>
2018-02-27 14:40:34 +0100

    fixes+ scimple.showAndBlock() available

--- testSimple2D.py
+++
@@ -1,5 +0,0 @@
-import scimple.scimple as scimple
-grapheneTable=scimple.ImportTable("test.xyz",firstLine=104,lastLine=495)
-myPlot2D=scimple.CreatePlot(2,xlabel="X",zlabel="Z",borders=[-20,20,18,19],title="Test Graphe 2D")
-myPlot2D.addToPlot(grapheneTable,3,4,label="graphene Y/Z",color="#ff0000")
-myPlot2D.addToPlot(grapheneTable,2,4,label="graphene X/Z",color="#ffff00")
