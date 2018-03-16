5446ac653d52d77d84504ed2462634f26215afa7
Bonnal Enzo <enzo.bonnal@orange.com>
2018-03-16 22:55:13 +0100

    fusion lex.py - scimple.py

--- scimple/setup.py
+++ scimple/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scimple',
-    version='1.1',
+    version='1.2',
     packages=['scimple_data'],
 	py_modules=['scimple'],
 	package_data={'scimple_data': ['*']},
