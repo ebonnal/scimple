eca793b51f2ae8a5215cfb380964384cf9be05d3
EnzoBnl <enzobonnal@gmail.com>
2019-06-06 16:58:56 +0200

    Update pygal_utils.py

--- scimple/pygal_utils.py
+++ scimple/pygal_utils.py
@@ -19,11 +19,21 @@ html_pygal="""
   </body>
 </html>
 """
+fig_pygal="""
+<figure>
+  <!-- Pygal render() result: -->
+  {a}
+  <!-- End of Pygal render() result: -->
+</figure>
+"""
 def show(chart):
     plot_html = html_pygal.format(a=chart.render(is_unicode=True))
     display(HTML(plot_html))
 def to_html(chart):
     plot_html = html_pygal.format(a=chart.render(is_unicode=True))
     return plot_html
+def to_html_fig(chart):
+    plot_html_fig = fig_pygal.format(a=chart.render(is_unicode=True))
+    return plot_html_fig
 def to_svg(chart):
     return chart.render(is_unicode=True)
